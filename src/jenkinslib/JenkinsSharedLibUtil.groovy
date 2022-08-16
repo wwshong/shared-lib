caa1ffecd51b08ab7d21d0086c67c1315820e15d
wwshong <61893599+wwshong@users.noreply.github.com>
2022-08-16 08:32:09 -0700

    Add files via upload

---
+++ src/jenkinslib/JenkinsSharedLibUtil.groovy
@@ -0,0 +1,17 @@
+package jenkinslib
+
+  public class JenkinsSharedLibUtil {
+    def sayHi() {
+   
+      println "in JenkinsSharedLibUtil.sayHi()"
+     
+    return "in JenkinsSharedLibUtil.sayHi()"; 
+  }
+  
+   def sayHi2() {
+   
+      println "in JenkinsSharedLibUtil.sayHi2()"
+     
+    return "in JenkinsSharedLibUtil.sayHi2()"; 
+  }
+}
