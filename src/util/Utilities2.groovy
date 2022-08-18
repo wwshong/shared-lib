271d13330c8cf2ab2f8951e7f64d75500b65c22e
wwshong <61893599+wwshong@users.noreply.github.com>
2022-08-18 12:40:03 -0700

    Update Utilities2.groovy

--- src/util/Utilities2.groovy
+++ src/util/Utilities2.groovy
@@ -4,5 +4,6 @@ class Utilities2 implements Serializable {
   Utilities2(steps) {this.steps = steps}
   def mvn(args) {
     steps.sh "echo Utilities2 arg = ${args}"
+	steps.sh "echo script.env.HOME=${script.env.HOME}"
   }
 }
