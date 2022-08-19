ccc561f339cb7259675b5310cbccaded741ad7bb
wwshong <61893599+wwshong@users.noreply.github.com>
2022-08-19 09:08:37 -0700

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,5 +1,7 @@
 import calculator.Calculator
 
 int add(int a, int b) {
+	echo "calculator in vars echo = $a + $b"
+	println "calculator in vars println = $a + $b"
   return new Calculator(script: this).add(a, b)
 }
