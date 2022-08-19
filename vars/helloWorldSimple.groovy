8687111606029d83dfe8e402aa86189dcdc256a6
wwshong <61893599+wwshong@users.noreply.github.com>
2022-08-18 20:01:47 -0700

    Update helloWorldSimple.groovy

--- vars/helloWorldSimple.groovy
+++ vars/helloWorldSimple.groovy
@@ -1,3 +1,9 @@
+import jenkinslib.JenkinsSharedLibUtil
+def jenkinsSharedLibUtil = new JenkinsSharedLibUtil()
 def call(String name, String dayOfWeek) {
   sh "echo Hello World ${name}. It is ${dayOfWeek}."
+  def s1 = jenkinsSharedLibUtil.sayHi()
+                    def s2 = jenkinsSharedLibUtil.sayHi2()
+                    println "helloWorldSimple s1=${s1}"
+                    println "helloWorldSimple s2=${s2}"
 }
