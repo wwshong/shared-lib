2581e129cb1465d1bd43fe392606cb35f06cdf02
wwshong <61893599+wwshong@users.noreply.github.com>
2022-08-19 08:57:18 -0700

    Update Calculator.groovy

--- src/calculator/Calculator.groovy
+++ src/calculator/Calculator.groovy
@@ -1,9 +1,10 @@
 package calculator
 
-class Calculator implements Serializable {
+class Calculator implements Serializable, ICalculator {
   // Jenkinsfile script context, note that all pipeline steps must use this context
   Object script = null
 
+  @Override
   int add(int a, int b) {
     // Note the script context is required for `echo`, as it is a pipeline step
     script.echo "Adding ${a} to ${b}"
